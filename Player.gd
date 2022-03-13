9735a53aa884bdb2d4adbdbde1b2e876dbdf7470
DanielGSilva <dg.sarmento2000@gmail.com>
2022-03-13 21:22:38 +0000

    Player and Background initialization

---
+++ Player.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -1
+	else:
+		velocity.y = 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+
+	position += velocity * delta
